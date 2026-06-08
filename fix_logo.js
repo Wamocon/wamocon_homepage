1fd80cd5736e1adef34928dd06829ceccdbfdb2d
Yash Bhesaniya <yash.bhesaniya@wamocon.com>
2026-06-08 09:17:06 +0200

    Update image references across multiple pages to use new logo assets
    - Changed og:image and logo image sources in page61094143.html, page64750211.html, page69503661.html, page70444849.html to point to the new logo image: images/tild3263-3339-4233-a130-316461303262__wmctransaprent_2.png.

--- fix_logo.js
+++ fix_logo.js
@@ -7,11 +7,11 @@ let count = 0;
 for (const file of files) {
     let html = fs.readFileSync(file, 'utf8');
     
-    // The logo is: <img class="t420__logo t-img" src="images/tild3038-3363-4533-a261-363563393531__tilda_logo_wmc_neu_0.png" imgfield="img" style="max-width: 200px;" alt="">
-    if (html.includes('max-width: 200px;') && html.includes('tilda_logo_wmc_neu_0.png')) {
+    // The logo is: <img class="t420__logo t-img" src="images/tild3263-3339-4233-a130-316461303262__wmctransaprent_2.png" imgfield="img" style="max-width: 200px;" alt="">
+    if (html.includes('max-width: 200px;') && html.includes('tild3263-3339-4233-a130-316461303262__wmctransaprent_2.png')) {
         // We only want to replace the max-width of the logo!
-        html = html.replace(/(<img[^>]*tilda_logo_wmc_neu_0\.png[^>]*style="max-width: )200px(;?"[^>]*>)/g, '$1130px$2');
-        html = html.replace(/(<img[^>]*tilda_logo_wmc_neu_0\.png[^>]*style="max-width: )220px(;?"[^>]*>)/g, '$1130px$2');
+        html = html.replace(/(<img[^>]*tild3263-3339-4233-a130-316461303262__wmctransaprent_2\.png[^>]*style="max-width: )200px(;?"[^>]*>)/g, '$1130px$2');
+        html = html.replace(/(<img[^>]*tild3263-3339-4233-a130-316461303262__wmctransaprent_2\.png[^>]*style="max-width: )220px(;?"[^>]*>)/g, '$1130px$2');
         fs.writeFileSync(file, html);
         count++;
     }
